768f910deb3e0e921840b3398ba767accce1ac25
stvhrs <steve.harris250105@gmail.com>
2024-09-22 05:37:01 +0700

    Bytes Shared Pref

--- lib/input/input.dart
+++ lib/input/input.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/services.dart';
 import 'package:flutter_quill/flutter_quill.dart';
 import 'package:flutter_quill_to_pdf/converter/configurator/converter_option/pdf_page_format.dart';
 import 'package:flutter_quill_to_pdf/converter/pdf_converter.dart';
@@ -5,15 +6,15 @@ import 'package:gmu/input/component/rich_text_field.dart';
 import 'package:gmu/state_management.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
+import 'package:file_picker/file_picker.dart';
 import 'component/textfield_caption.dart';
 
 class InputBab extends StatelessWidget {
   final TextEditingController _footerConttoler = TextEditingController();
   final TextEditingController _babConttoler = TextEditingController();
   final TextEditingController _judulBabConttoler = TextEditingController();
-    final TextEditingController _tujuuanConttoler = TextEditingController();
-final QuillController _quillController=QuillController.basic();
+  final TextEditingController _tujuuanConttoler = TextEditingController();
+  final QuillController _quillController = QuillController.basic();
 
   InputBab({super.key});
   @override
@@ -49,33 +50,35 @@ final QuillController _quillController=QuillController.basic();
             )),
         InputCaption(
             caption: "Tujuan",
-            child: TextFormField(controller: _tujuuanConttoler  ,
+            child: TextFormField(
+              controller: _tujuuanConttoler,
               maxLines: 10,
             )),
-         Consumer<BooksProvider>(
-          
-          builder: (context, book,c) {
-            return InputCaption(
-                caption: "PetaKonsep",
-                child:book.selectedImage!=null?Image.network( book.selectedImage!.path) :IconButton(onPressed: (){
-                 
-                  book.pickImage();
-                   
-                }, icon: Icon(Icons.browse_gallery)));
-          }
-        ),
-           InputCaption(caption: "Rich Text", child: SizedBox(height: 500,child: RichTextField(controller:_quillController))),
+        Consumer<BooksProvider>(builder: (context, book, c) {
+          return InputCaption(
+              caption: "PetaKonsep",
+              child: book.selectedImage != null
+                  ? Image.memory(book.selectedImage!)
+                  : IconButton(
+                      onPressed: () async {
+                        book.pickImage();
+                      },
+                      icon: Icon(Icons.browse_gallery)));
+        }),
+        InputCaption(
+            caption: "Rich Text",
+            child: SizedBox(
+                height: 500,
+                child: RichTextField(controller: _quillController))),
         FloatingActionButton(
             backgroundColor: Colors.green,
             child: const Icon(Icons.refresh),
             onPressed: () {
-
-
               var prov = Provider.of<BooksProvider>(context, listen: false);
               prov.inputJudulFooter = _footerConttoler.text;
               prov.inputJudulBab = _judulBabConttoler.text;
               prov.inputBab = _babConttoler.text;
-                         prov.inputTujuan = _tujuuanConttoler.text;
+              prov.inputTujuan = _tujuuanConttoler.text;
             }),
       ]),
     );
